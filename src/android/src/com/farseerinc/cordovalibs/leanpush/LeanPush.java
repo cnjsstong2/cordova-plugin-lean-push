186bc78cac9e2aee390067032c953ab7525beb71
Tong Shen <tshen@farseerinc.com>
2015-05-01 16:04:25 -0400

    Fix android initialization.

--- src/android/src/com/farseerinc/cordovalibs/leanpush/LeanPush.java
+++ src/android/src/com/farseerinc/cordovalibs/leanpush/LeanPush.java
@@ -1,16 +1,46 @@
 package com.farseerinc.cordovalibs.leanpush;
 
+import android.util.Log;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVInstallation;
+import com.avos.avoscloud.PushService;
 import com.avos.avoscloud.SaveCallback;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.Override;
+
 public class LeanPush extends CordovaPlugin {
+
+    private static CordovaWebView mWebView;
+    private static String mECB;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Log.d("LeanPush", "initialize");
+        AVInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                AVInstallation.getCurrentInstallation().saveInBackground();
+                if (e == null) {
+                    String installationId = AVInstallation.getCurrentInstallation().getInstallationId();
+                    Log.d("LeanPush", "got installationId: " + installationId);
+                } else {
+                    Log.d("LeanPush", "fail to get installationId", e);
+                }
+            }
+        });
+        PushService.setDefaultPushCallback(cordova.getActivity(), cordova.getActivity().getClass());
+        mECB = null;
+        mWebView = this.webView;
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -22,18 +52,11 @@ public class LeanPush extends CordovaPlugin {
     }
 
     private void getInstallation(final CallbackContext callbackContext) {
-        AVInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
-            public void done(AVException e) {
-                if (e == null) {
-                    // 保存成功
-                    String installationId = AVInstallation.getCurrentInstallation().getInstallationId();
-                    callbackContext.success("android," + installationId);
-                    // 关联  installationId 到用户表等操作……
-                } else {
-                    // 保存失败，输出错误信息
-                    callbackContext.error("Fail to get Installation.");
-                }
-            }
-        });
+        String installationId = AVInstallation.getCurrentInstallation().getInstallationId();
+        if (installationId == null) {
+            callbackContext.error("Fail to get Installation.");
+        } else {
+            callbackContext.success("android," + installationId);
+        }
     }
 }
